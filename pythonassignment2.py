5bc712e4009a38bd4092ead00f6485792b92101f
Debbie-02 <deborahmadu4@gmail.com>
2025-08-11 03:06:52 +0100

    Python Assignment Week 2

---
+++ pythonassignment2.py
@@ -0,0 +1,29 @@
+# Python code to manipulate a list as per the given instructions
+
+#Create an empty list
+my_list = []
+
+#Append elements to the list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+#Insert 15 at the second position
+my_list.insert(1, 15)
+
+#Extend the list with another list
+my_list.extend([50, 60, 70])
+
+#Remove the last element
+my_list.pop()
+
+#Sort my_list in ascending order
+my_list.sort()
+
+#Find and print the index of 30
+index = my_list.index(30)
+print("Index of 30:", index)
+
+#print the final list
+print("Final list:", my_list)
